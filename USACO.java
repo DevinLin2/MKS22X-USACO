8fd96762c1570d7eca35290eec85064045b76d68
Devin Lin <Dlin01@stuy.edu>
2019-03-06 13:56:52 -0500

    added skeleton of functions

---
+++ USACO.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class USACO {
+
+  private int row;
+  private int col;
+  private int elevation;
+  private int numOfInstructions;
+  private int[][] lake;
+
+  public static void main(String[] args) {
+    // do something
+  }
+
+  public static int bronze(String filename) {
+    return -1; // so it compiles
+  }
+
+  public static int silver(String filename) {
+    return -1; // so it compiles
+  }
+
+  private static void readfile(String filename) {
+    
+  }
+}
